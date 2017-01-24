1cbd3fa77fcf392ab01ae72f84e5de86c02fa262
clairellesage <clairellesage@gmail.com>
2017-01-24 05:32:25 +0000

    fixed html to span class instead of button

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -17,29 +17,30 @@ module.exports = function(DataHelpers) {
     });
   });
 
-  tweetsRoutes.post("/tweets", function(req, res) {
-    if (!req.body.text) {
-      res.status(400).json({ error: 'invalid request: no data in POST body'});
-      return;
-    } 
-    const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
-    const tweet = {
-      user: user,
-      content: {
-        text: req.body.text
-      },
-      created_at: Date.now()
-    };
+   tweetsRoutes.post("/", function(req, res) {
+     if (!req.body.text) {
+       res.status(400).json({ error: 'invalid request: no data in POST body'});
+       return;
+     }
 
-    DataHelpers.saveTweet(tweet, (err) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-      } else {
-        res.status(201).send();
-      }
-    });
-  });
+     const user = req.body.user ? req.body.user : userHelper.generateRandomUser();
+     const tweet = {
+       user: user,
+       content: {
+         text: req.body.text
+       },
+       created_at: Date.now()
+     };
 
-  return tweetsRoutes;
+     DataHelpers.saveTweet(tweet, (err) => {
+       if (err) {
+         res.status(500).json({ error: err.message });
+       } else {
+         res.status(200).send();
+       }
+     });
+   });
 
-}
+   return tweetsRoutes;
+
+ }
