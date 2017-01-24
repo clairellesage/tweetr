d51fd7ccb43247aa2ef1319e721d42165d94ade3
clairellesage <clairellesage@gmail.com>
2017-01-24 23:00:51 +0000

    css for tweets

--- public/scripts/composer-character-counter.js
+++ public/scripts/composer-character-counter.js
@@ -1,40 +1,12 @@
-
 $(document).ready(function() {
   	console.log("ready!");
   	let max = $('.counter').text();
   	$('#text').bind("keyup", "keydown", function(event) {
-  		let result = $('.counter').text(max - this.value.length)
+  		let newVal = (max - this.value.length)
+  		let result = $('.counter').text(newVal)
+  		if (newVal < 0) {
+  			$('.counter').addClass("negative");
+  		}
   		return result;
   	});
 });
-
-// console.log($(document).className('.counter'))
-
-
-// var counter = $(document).querySelector('.counter')
-// conosle.log(counter)
-
-// $textarea.addEventListener("keydown", function (event) {
-// 	var max = 140;
-// 	if (event) {
-// 		max -= 1;
-// 	}
-// 	return max;
-// })
-// console.log(max);
-// var textarea = $('#text');
-// document.addEventListener("keydown", function (event) {
-// 	console.log(event)
-// })
-
-// document.addEventListener("dblclick", function(event) {
-//   console.log(event);
-// });
-
-//on backspace, add characters
-//key location
-
-//js and the dom do this
-// function $(query){
-// 	return document.querySelector(query);
-// }
