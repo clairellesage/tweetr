bc4157c5697ae0f26825123bb6a57590e8e3462f
clairellesage <clairellesage@gmail.com>
2017-01-24 03:34:33 +0000

    init commit

---
+++ public/scripts/composer-character-counter.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const userHelper    = require("../lib/util/user-helper")
+const express       = require('express');
+const tweetsRoutes  = express.Router();
+
+module.exports = function(characterCounter) {
+
+}
+
+$(document).ready(function() {
+    console.log( "ready!" );
+});
