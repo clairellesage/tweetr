7b6bbdf53bc89af49cf48bf1c0a4b65bd646531f
clairellesage <clairellesage@gmail.com>
2017-01-24 17:24:53 +0000

    added live counter

--- public/scripts/composer-character-counter.js
+++ public/scripts/composer-character-counter.js
@@ -1,11 +1,40 @@
-// "use strict";
 
-// const express       = require('express');
-// const tweetsRoutes  = express.Router();
+$(document).ready(function() {
+  	console.log("ready!");
+  	let max = $('.counter').text();
+  	$('#text').bind("keyup", "keydown", function(event) {
+  		let result = $('.counter').text(max - this.value.length)
+  		return result;
+  	});
+});
 
-// module.exports = function(characterCounter) {
-// 	$(document).ready(function() {
-//     	console.log( "ready!" );
-// 	});
+// console.log($(document).className('.counter'))
+
+
+// var counter = $(document).querySelector('.counter')
+// conosle.log(counter)
+
+// $textarea.addEventListener("keydown", function (event) {
+// 	var max = 140;
+// 	if (event) {
+// 		max -= 1;
+// 	}
+// 	return max;
+// })
+// console.log(max);
+// var textarea = $('#text');
+// document.addEventListener("keydown", function (event) {
+// 	console.log(event)
+// })
+
+// document.addEventListener("dblclick", function(event) {
+//   console.log(event);
+// });
+
+//on backspace, add characters
+//key location
+
+//js and the dom do this
+// function $(query){
+// 	return document.querySelector(query);
 // }
-
