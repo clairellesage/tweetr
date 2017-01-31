896281625956f25dbb97a68c836ec6b544439eff
Faisal Al-Tameemi <faisal.al.tameemi@gmail.com>
2017-01-31 17:36:08 -0500

    added mongodb dataHelper notes

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,27 +1,27 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
+  const saveTweet = function(newTweet, callback) {
+      db.collection("tweets")
+        .insertOne(newTweet, callback);
+  };
+
   return {
-
     // Saves a tweet to `db`
-    saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
-    },
+    saveTweet: saveTweet,
 
-    // Get all tweets in `db`, sorted by newest first
+    // NOTE: keep exported object readabe, refactor
+    //       method below, similar to `saveTweet`
+    // loadds all tweets from DB
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      const sortNewestFirst = (a, b) => a.created_at - b.created_at;
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets.sort(sortNewestFirst));
       });
     }
-
-  };
+  }
 }
